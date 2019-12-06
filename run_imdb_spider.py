757f604374c358f26a824111fac84a0b650574b1
karoly-hars <harsanyika@gmail.com>
2019-12-06 20:36:20 +0100

    add output path to args

--- run_imdb_spider.py
+++ run_imdb_spider.py
@@ -11,13 +11,15 @@ def get_arguments():
                         help='Keywords from the TV show title. '
                              'Ideally they should be lowercase and whitspace separated.'
                              '\nExamples: \"star trek\" or \"rick and morty\"')
+    parser.add_argument('-o', '--output_path', type=str, required=False, default='imdb_episode_summaries.json',
+                        help='Path to the output JSON file.')
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
     args = get_arguments()
-    call = 'scrapy crawl imdb_episode_summary_spider -a title_keywords="{}" -o imdb_episode_summaries.json'.format(
-        ' '.join(args.title_keywords)
-    )
+
+    call = 'scrapy crawl imdb_episode_summary_spider -a title_keywords="{}" -o {}'.format(' '.join(args.title_keywords),
+                                                                                          args.output_path)
     os.system(call)
--- run_wiki_spider.py
+++ run_wiki_spider.py
@@ -7,32 +7,37 @@ def get_arguments():
         description="Wikipedia episode summary parser.",
         formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=75)
     )
-    parser.add_argument('-s', '--start_url', type=str,
+    parser.add_argument('-s', '--start_url', type=str, required=True,
                         help='start URL for the spider.'
                              '\nShould be: '
                              'https://en.wikipedia.org/wiki/<Show_Title_With_Underscores_And_Capitalized_Init_Chars>'
                              '\nExample: https://en.wikipedia.org/wiki/Star_Trek')
-    parser.add_argument('-a', '--allow', type=str,
+    parser.add_argument('-a', '--allow', type=str, required=True,
                         help='Wikipedia urls must include this substring otherwise the spider will not enter the URL.'
                              '\nIdeally, it should be: <Show_Title_With_Underscores_And_Capitalized_Init_Chars>'
                              '\nExample: \"Star_Trek\"')
-    parser.add_argument('-d', '--deny', type=str,
+    parser.add_argument('-d', '--deny', type=str, required=False, default=None,
                         help='The Wikiepdia spider will NOT examine the pages which contain '
                              'any of these substrings in their urls.')
-    parser.add_argument('-t', '--title_keywords', nargs='*',
+    parser.add_argument('-t', '--title_keywords', nargs='*', required=False, default='',
                         help='The title of the Wikipedia page must include these keywords, '
                              'otherwise the spider will not extract anything from the page.'
                              '\n Good practice: use the lowercase version of the words from the show\'s title'
-                             '\n Example: star trek')
+                             '\n Example: Star Trek')
+    parser.add_argument('-o', '--output_path', type=str, required=False, default='wiki_episode_summaries.json',
+                        help='Path to the output JSON file.')
+
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
     args = get_arguments()
+
     call = 'scrapy crawl wiki_episode_table_spider -a start_url=\"{}\" -a allow=\"{}\" -a deny=\"{}\" ' \
-           '-a title_keywords=\"{}\" -o wiki_episode_summaries.json'.format(args.start_url,
-                                                                            args.allow,
-                                                                            args.deny,
-                                                                            ' '.join(args.title_keywords))
+           '-a title_keywords=\"{}\" -o {}'.format(args.start_url,
+                                                   args.allow,
+                                                   args.deny,
+                                                   ' '.join(args.title_keywords),
+                                                   args.output_path)
     os.system(call)
