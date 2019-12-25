1315b5e83ba96adfd5fc42659fea03a873dfcd4b
karoly-hars <harsanyika@gmail.com>
2019-12-25 22:13:20 +0100

    changes to model loading in generate.py

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import argparse
 import torch
 from dataset import EpisodeSummaryTokenizer
 from utils import set_random_seeds, generate_sequence
-from pytorch_transformers import GPT2LMHeadModel
+from pytorch_transformers import GPT2Config, GPT2LMHeadModel
 
 
 def generate_samples(args):
@@ -13,7 +13,9 @@ def generate_samples(args):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # Load pre-trained network weights
-    model = GPT2LMHeadModel.from_pretrained(args.gpt2_version)
+    config = GPT2Config.from_pretrained(args.gpt2_version)
+    model = GPT2LMHeadModel(config)
+    model.load_state_dict(torch.load(args.model_load_path))
     model = model.to(device)
     model.eval()
 
@@ -34,7 +36,7 @@ def get_arguments():
     parser.add_argument("-g", "--gpt2_version", type=str, required=False, default="gpt2-medium",
                         choices=["gpt2, gpt2-medium", "gpt2-large"],
                         help="Which GPT2 version to use from pytorch-transformers.")
-    parser.add_argument("-sp", "--model_save_path", type=str, required=False, default="ep_summary_gen_model.pth",
+    parser.add_argument("-sp", "--model_load_path", type=str, required=False, default="ep_summary_gen_model.pth",
                         help="Save path for the trained model or checkpoints during training.")
     parser.add_argument("-ns", "--num_samples", type=int, required=False, default=8,
                         help="Number of samples generated and displayed at every checkpoint.")
