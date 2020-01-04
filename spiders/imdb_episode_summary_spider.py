c54b572709b676201a4e4b567ee4cb0566e0a2e0
karoly-hars <harsanyika@gmail.com>
2020-01-04 22:57:54 +0100

    simplify imdb spider

--- spiders/imdb_episode_summary_spider.py
+++ spiders/imdb_episode_summary_spider.py
@@ -19,8 +19,8 @@ class ImdbEpisodeSummarySpider(scrapy.Spider):
 
     def parse(self, response):
         """Parse and yield all relevant episode data from an IMDb page."""
-        tv_show_links = response.xpath('//td[@class="result_text"]/a/@href').extract()
-        tv_show_titles = response.xpath('//td[@class="result_text"]').extract()
+        tv_show_links = response.xpath('//*[@class="result_text"]/a/@href').extract()
+        tv_show_titles = response.xpath('//*[@class="result_text"]').extract()
 
         for tv_show_title, tv_show_link in zip(tv_show_titles, tv_show_links):
             tv_show_title = BeautifulSoup(tv_show_title, 'lxml').get_text().strip()
@@ -34,37 +34,34 @@ class ImdbEpisodeSummarySpider(scrapy.Spider):
 
     def parse_tv_show_page(self, response):
         """Look up the episode list urls."""
-        ratings_wrapper = response.xpath('//div[@class="ratings_wrapper"]')
+        # get the rating count. if its > 500, this might be a real TV show, not some fan-made project
+        # this does not work all the time, but in my experience it might be a good indicator
+        rating_count = response.xpath('//*[@itemprop="ratingCount"]/text()').extract_first()
 
-        if ratings_wrapper:
-            # get the rating count. if its > 500, this might be a real TV show, not some fan-made project
-            # this does not work all the time, but in my experience it might be a good indicator
-            rating_count = ratings_wrapper.xpath('//span[@itemprop="ratingCount"]/text()').extract_first()
+        if rating_count:
+            rating_count = int(rating_count.replace(',', ''))
 
-            if rating_count:
-                rating_count = int(rating_count.replace(',', ''))
+            if rating_count > 500:
+                # look up episodes
+                episode_list_urls = response.xpath('//*[@class="seasons-and-year-nav"]/div/a/@href').extract()
 
-                if rating_count > 500:
-                    # look up episodes
-                    episode_list_urls = response.xpath('//div[@class="seasons-and-year-nav"]/div/a/@href').extract()
+                episode_list_urls = [response.urljoin(e) for e in episode_list_urls if 'season' in e]
 
-                    episode_list_urls = [response.urljoin(e) for e in episode_list_urls if 'season' in e]
-
-                    for url in episode_list_urls:
-                        yield scrapy.Request(url, callback=self.parse_episode_list)
+                for url in episode_list_urls:
+                    yield scrapy.Request(url, callback=self.parse_episode_list)
 
     def parse_episode_list(self, response):
         """Look up the episode urls from an episode list page."""
-        episode_list = response.xpath('//div[@class="list detail eplist"]')
-        episode_page_urls = episode_list.xpath('//div[@class="info"]/strong/a/@href').extract()
+        episode_list = response.xpath('//*[@class="list detail eplist"]')
+        episode_page_urls = episode_list.xpath('//*[@class="info"]/strong/a/@href').extract()
         episode_page_urls = ['https://www.imdb.com{}plotsummary'.format(e) for e in episode_page_urls]
 
         for url in episode_page_urls:
             yield scrapy.Request(url, callback=self.parse_plot_summary_page)
 
         # look up the page for the previous/next season
-        prev = response.xpath('//div/a[@id="load_previous_episodes"]/@href').extract_first()
-        next = response.xpath('//div/a[@id="load_next_episodes"]/@href').extract_first()
+        prev = response.xpath('//*[@id="load_previous_episodes"]/@href').extract_first()
+        next = response.xpath('//*[@id="load_next_episodes"]/@href').extract_first()
 
         base_url = response.url
         base_url = base_url.split("episodes?")[0]
@@ -78,9 +75,9 @@ class ImdbEpisodeSummarySpider(scrapy.Spider):
 
     def parse_plot_summary_page(self, response):
         """Create and load the episode summary items."""
-        show_title = response.xpath('//div[@class="subpage_title_block"]//h4/a/text()').extract_first().strip()
-        ep_title = response.xpath('//div[@class="subpage_title_block"]//h3/a/text()').extract_first().strip()
-        summaries = response.xpath('//ul[@id="plot-summaries-content"]/li[@class="ipl-zebra-list__item"]/p').extract()
+        show_title = response.xpath('//*[@class="subpage_title_block"]//h4/a/text()').extract_first().strip()
+        ep_title = response.xpath('//*[@class="subpage_title_block"]//h3/a/text()').extract_first().strip()
+        summaries = response.xpath('//*[@class="ipl-zebra-list__item" and contains(@id, "summary")]/p').extract()
 
         for ep_sum in summaries:
             ep_sum = BeautifulSoup(ep_sum, 'lxml').get_text().strip()
